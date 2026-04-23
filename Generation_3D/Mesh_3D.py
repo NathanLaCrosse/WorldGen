6b6a751ca7fec1d6f65b51a1a347db42348c8bdc
Dylan Dudley <dudleyd8828@my.uwstout.edu>
2026-04-23 13:53:57 -0500

    Fixed Mesh Generation

--- Generation_3D/Mesh_3D.py
+++ Generation_3D/Mesh_3D.py
@@ -2,6 +2,18 @@ from ursina import *
 from PNGConvert.MeshGrid import get_color
 from ursina.shaders import lit_with_shadows_shader
 
+
+def neighbor_check(world, x, y ,z):
+    if z < 0 or z >= len(world):
+        return True
+    if y < 0 or y >= len(world[0]):
+        return True
+    if x < 0 or x >= len(world[0][0]):
+        return True
+
+    return world[z][y][x] == 0  # adjust if your "empty" value differs
+    return
+
 def create_voxel_mesh(world_3d, index_to_color_3d):
     vertices = []
     triangles = []
@@ -9,39 +21,7 @@ def create_voxel_mesh(world_3d, index_to_color_3d):
     index_offset = 0
     depth = len(world_3d)
 
-    # For how many layers we have in our 3D world
-    for z in range(depth):
-        layer = world_3d[z]
-        rows = len(layer)
-
-        # Since when we input sample data we have a X by X image, must make sure it is the same
-        if index_to_color_3d is None:
-             rows = len(layer[0])
-
-        cols = len(layer[0])
-
-        for y in range(rows):
-            for x in range(cols):
-
-                gx = x
-                gy = z
-                gz = y
-
-                # Sets up the cubes
-                cube_verts = [
-                    Vec3(gx, gy, gz),
-                    Vec3(gx+1, gy, gz),
-                    Vec3(gx+1, gy+1, gz),
-                    Vec3(gx, gy+1, gz),
-                    Vec3(gx, gy, gz+1),
-                    Vec3(gx+1, gy, gz+1),
-                    Vec3(gx+1, gy+1, gz+1),
-                    Vec3(gx, gy+1, gz+1),
-                ]
-
-                vertices.extend(cube_verts)
-
-                cube_tris = [
+    cube_tris = [
                     0,1,2, 0,2,3,
                     5,4,7, 5,7,6,
                     4,0,3, 4,3,7,
@@ -49,23 +29,44 @@ def create_voxel_mesh(world_3d, index_to_color_3d):
                     3,2,6, 3,6,7,
                     4,5,1, 4,1,0
                 ]
+    
+    # For how many layers we have in our 3D world
+    for z in range(depth):
+        layer = world_3d[z]
+        rows = len(layer)
+
+
+        cols = len(layer[0])
+
+        for y in range(rows):
+            for x in range(cols):
+
+                gx = float(x)
+                gy = float(z)
+                gz = float(y)
+
+                color = get_color(world_3d, x, y, z, index_to_color_3d)
+
+                if (color[0] == 0 and color[1] == 0 and color[2] == 0):
+                    continue
+
+                colors.extend([color] * 8)
+
+                # Sets up the cubes
+                cube_verts = [
+                    (gx, gy, gz),
+                    (gx+1, gy, gz),
+                    (gx+1, gy+1, gz),
+                    (gx, gy+1, gz),
+                    (gx, gy, gz+1),
+                    (gx+1, gy, gz+1),
+                    (gx+1, gy+1, gz+1),
+                    (gx, gy+1, gz+1),
+                ]
+
+                vertices.extend(cube_verts)
 
                 triangles.extend([i + index_offset for i in cube_tris])
-
-                # Logic to get the color for the current voxel
-                if index_to_color_3d is not None:
-                    colors.extend([get_color(world_3d, x, y, z, index_to_color_3d)] * 8)
-                else:
-                    # This does the r,g,b to normalized, and also handles the transparent case for sample
-                    r,g,b = layer[0][y][x]
-                    r = r / 255
-                    g = g / 255
-                    b = b / 255
-                    
-                    if r == 0 and g == 0 and b == 0:
-                        colors.extend([color.rgba(0, 0, 0, 0)] * 8)  # fully transparent
-                    else:
-                        colors.extend([color.rgb(r, g, b)] * 8)
 
                 index_offset += 8
 
--- Main_WFC.py
+++ Main_WFC.py
@@ -3,32 +3,29 @@ from Generation_3D.Main_3D import ThreeD_Main, new_3D_main
 from ursina import *
 import numpy as np
 
-# General parms
-grid_size = 15 # output grid size
-tile_size = 2 # For sample tiles
-rotation = False # If we want rotations allowed
-png_name = "cityScape" # Name of the PNG file in the images folder
 
-# 2-D only Parms
-wave = True # If true, uses the single grid generation, if false uses the chunk based generation
+# 3-D only parms 
+gen_size = (25,25,25)
 stride = 1 
 chunks = 1 # number of chunks to split the map into for better performance
 
-# Make sure that tile_size = num_images when doing sample only and rotation = false
-# 3-D only parms 
-num_images = 35 # Number of images to use for the WFC, should be between 1 and 7
-sample_only = True # If true, only creates the sample image, if false creates the WFC output (note: if true, it will not create the WFC output, even if wave is true, since it needs the WFC output to create the sample image)
-png_folder = "Generation_3D/images_3D/cityScape"  # Mount is 30x30 image with 15 images. CityScape is 35x35x35
-rows = 35
-cols = 35
-Surface_start = 0 #For caves only
-gen_size = (15,30,30)
+# -------------------------------------------------------------------------------------------------------------------------------------
+#
+#3-D parms  Num images,     png foler,                    png name, rotations, tile_size, row, cols, surface_start, gen_size, preset
+#
+# -------------------------------------------------------------------------------------------------------------------------------------
+cityScape_parms = (33, "Generation_3D/images_3D/cityScape","cityScape",False,      2,      35, 35,         1,       gen_size, True)
+mount_parms = (    15,     "Generation_3D/images_3D/mount","mount"    ,False,      2,      30, 30,         9,       gen_size, True)
+grass = (          5,      "Generation_3D/images_3D/mount","mount"    ,True ,      2,      5,  5,          0,       gen_size, False)
+cave = (           7,      "Generation_3D/images_3D/mount","mount"    ,False,      2,      7,  7,          0,       gen_size, False)
+richgrass = (      8,      "Generation_3D/images_3D/mount","mount"    ,True ,      2,      8,  8,          0,       gen_size, False)
 
-# Could work on adding in a function to fix the base layer to the base layer of the WFC
-#fixed_base = False
+
+# Set to what image you want to load
+parms = mount_parms
 
 # Main 3D WFC handler
-image = new_3D_main(gen_size, tile_size, stride, num_images, png_folder, png_name, rotation, chunks, rows, cols, Surface_start)
+image = new_3D_main(parms[8], parms[4], stride, parms[0], parms[1], parms[2], parms[3], chunks, parms[5], parms[6], parms[7], parms[9])
 
 # 2-D WFC
 # RunPNG(
@@ -55,9 +52,9 @@ image = new_3D_main(gen_size, tile_size, stride, num_images, png_folder, png_nam
 app = Ursina()
 
 # Calculate the true center of the mesh in world space
-center_x = grid_size / 2        # gx goes 0 to cols
-center_y = grid_size / 2  # gy goes 0 to depth (num_images)
-center_z = grid_size / 2        # gz goes 0 to rows
+center_x = gen_size[1] / 2        # gx goes 0 to cols
+center_y = gen_size[0] / 2  # gy goes 0 to depth (num_images)
+center_z = gen_size[2] / 2        # gz goes 0 to rows
 
 pivot = Entity(x=center_x, y=center_y, z=center_z)
 image.parent = pivot
@@ -66,12 +63,12 @@ image.y = -center_y
 image.z = -center_z
 
 # Lighting
-DirectionalLight(x=grid_size/2, y=num_images+3, z=grid_size/2, shadows=True, rotation=(45, -45, 45)).look_at(Vec3(1, -1, -1))
+DirectionalLight(x=gen_size[1]/2, y=gen_size[0]+3, z=gen_size[2]/2, shadows=True, rotation=(45, -45, 45)).look_at(Vec3(1, -1, -1))
 AmbientLight(color=color.rgba(30, 30, 30, 0.5))
 
-camera_spot = grid_size * chunks / 2
-camera.position = (camera_spot, camera_spot, -(chunks * grid_size * 2))
-camera.look_at(Vec3(grid_size / 2, -5, 0))
+camera_spot = gen_size[0] * chunks / 2
+camera.position = (camera_spot, camera_spot, -(chunks * gen_size[1] * 2))
+camera.look_at(Vec3(gen_size[1] / 2, -5, 0))
 
 mouse.locked = True
 
@@ -83,8 +80,8 @@ def input(key):
     if key == 'q':
         application.quit()
     if key == 'backspace':
-        camera.position = (camera_spot, camera_spot, -(chunks * grid_size * 2))
-        camera.look_at(Vec3(grid_size / 2, -5, 0))
+        camera.position = (camera_spot, camera_spot, -(chunks * gen_size[1] * 2))
+        camera.look_at(Vec3(gen_size[1] / 2, -5, 0))
 
 def update():
     global rot_x, rot_y
