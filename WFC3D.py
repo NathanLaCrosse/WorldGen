62dea974dedee830d6bb0c08bdfd80f300b27b35
Nathan L <mancool5672@gmail.com>
2026-05-06 13:26:13 -0500

    Cleaned up some of the files. Made the saving/loading functions more robust.

--- WFC3D.py
+++ WFC3D.py
@@ -147,7 +147,7 @@ def generate_3D_fully_recursive(gen_size, tile_to_dex, dex_to_tile, weights, num
 
 def apply_boundary_constraints(cell_space, state_space, entropy_grid, queue, modifications, rev_adj, space_size, 
                                num_states, boundary_conditions):
-        
+    # Enforce boundary conditions between chunks
     for (d,r,c), neighbor_state, direction in boundary_conditions:
         if state_space[d,r,c] != -1:
             continue # Already collapsed
@@ -317,41 +317,13 @@ def build_grid_from_cell_space(state_space, gen_size, space_size, tile_size, num
 
     return grid
 
-if __name__ == "__main__":
-    # Generation settings
-    gen_size = (2,4,4)
-    chunk_size = (8,8,8)
-    tile_size = 2
-    stride = 1
-
-    # Loading settings are as follows:
-    #          load_adj, load_tilemap, rotate, folder_name, file_prefix, depth, rows, cols, save_name
-    # settings = (False, True, True, "AnotherMountain", "amount", 14, 32, 32, "diaorama2")
-    # settings = (False, True, True, "AnotherMountain", "amount", 14, 32, 32, "diaorama1")
-    # settings = (False, True, True, "SmolMountain", "mountain", 19, 32, 32, "diaorama3") # Fastest running example
-    # settings = (False, True, True, "OceanMountain3d", "omount", 9, 48, 48, "diaorama4")
-    # settings = (False, False, True, "OceanMountain3d", "omount", 9, 48, 48, "diaorama5")
-    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama6")
-    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama7") # Nice mini diaorama
-    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama8") # Another nice small one
-    settings = (True, False, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama9")
-
-    load_adjacences, load_tilemap, r, png_folder, png_names, png_depth, png_rows, png_cols, space_name = settings
-
-    png_folder = "Generation_3D/images_3D/" + png_folder
-
-    # Overrides
-    # load_adjacences = True
-    # load_tilemap = True
-    # r = True
-
-    rev_adj = None
-    if not load_adjacences:
+def gather_tilemap_data(png_depth, png_rows, png_cols, png_folder, png_names, tile_size=2, stride=1, rotate=False, save_dir=None, load_dir=None, filename=None):
+    if load_dir is None:
         tilemap, idx_to_color, color_to_idx = construct_3D_tilemap(png_depth,png_rows,png_cols,png_folder=png_folder, png_names=png_names)
 
-        tiles, weights = collect_3D_tiles(tilemap, tile_size, rotation=r)
+        tiles, weights = collect_3D_tiles(tilemap, tile_size, rotation=rotate)
         print("Tiles Collected!")
-
+        
         num_colors = len(idx_to_color.keys())
         num_states = len(tiles)
 
@@ -363,46 +335,107 @@ if __name__ == "__main__":
         rev_adj = collect_reverse_adjacencies(dex_to_tile, num_states, tile_size=tile_size, stride=stride)
         print("Adjacencies Built!")
 
-        with open("Saved_Data/data.pkl", "wb") as file:
-            pickle.dump([tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj], file)
-        print("Data Saved!")
-    
+        if save_dir is not None:
+            with open(f"{save_dir}/{filename}.pkl", "wb") as file:
+                pickle.dump([tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj], file)
+            print("Data Saved!")
+        
+        return tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj, num_colors, num_states
     else:
-        with open("Saved_Data/data.pkl", "rb") as file:
+        with open(f"{load_dir}/{filename}.pkl", "rb") as file:
             tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj = pickle.load(file)
         
         num_colors = len(idx_to_color.keys())
         num_states = len(tiles)
 
-        print("loaded!")
+        print("Data Loaded!")
 
-    if not load_tilemap:
-        # Set the first layer to stone
-        presets = []
-        space_size = ((chunk_size[0] - tile_size)//stride + 1, (chunk_size[1] - tile_size)//stride + 1, (chunk_size[2] - tile_size)//stride + 1)
-        # for i in range(space_size[1]):
-        #     for j in range(space_size[2]):
-        #         presets.append(((0, i, j), 0))
-        for i in range(1):
-            for j in range(1):
-                presets.append(((0, i, j), 0))
+        return tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj, num_colors, num_states
+    
+def generate_world(tile_data_tuple, tile_size=2, stride=1, gen_size=(2,2,2), chunk_size=(8,8,8), presets=None, save_dir=None, load_dir=None, filename=None):
+    # Grab all tilemap data from the tuple 
+    tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj, num_colors, num_states = tile_data_tuple
 
+    if load_dir is None:
         while True:
             space, res = generate_3D_chunks(gen_size, chunk_size, tile_to_dex, dex_to_tile, weights, num_colors, 
                 tile_size, stride, seeding_presets=presets, rev_adj=rev_adj, attemps_per_chunk=3, seed_bottom=False)
             
             if res:
                 break
-
-        with open(f"Saved_Data/{space_name}.pkl", "wb") as file:
-            pickle.dump(space, file)
+            else:
+                print("Invalid Point Reached, Restarting Generation")
+                
+        if save_dir is not None:
+            with open(f"{save_dir}/{filename}.pkl", "wb") as file:
+                pickle.dump(space, file)
+        
+        return space
     else:
-        with open(f"Saved_Data/{space_name}.pkl", "rb") as file:
+        with open(f"{load_dir}/{filename}.pkl", "rb") as file:
             space = pickle.load(file)
+        
+        return space
 
-    # space = space[::-1]
+if __name__ == "__main__":
+    # Generation settings
 
-    # create_voxel_mesh(tiles[0], idx_to_color)
+    # Loading settings are as follows:
+    #          load_adj, load_tilemap, rotate, folder_name, file_prefix, depth, rows, cols, save_name
+    # settings = (False, True, True, "AnotherMountain", "amount", 14, 32, 32, "diaorama2")
+    # settings = (False, True, True, "AnotherMountain", "amount", 14, 32, 32, "diaorama1")
+    # settings = (False, True, True, "SmolMountain", "mountain", 19, 32, 32, "diaorama3") # Fastest running example
+    # settings = (False, True, True, "OceanMountain3d", "omount", 9, 48, 48, "diaorama4")
+    # settings = (False, False, True, "OceanMountain3d", "omount", 9, 48, 48, "diaorama5")
+    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama6")
+    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama7") # Nice mini diaorama
+    # settings = (False, True, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama8") # Another nice small one
+    # settings = (False, False, True, "AnotherOceanMountain", "omount", 9, 32, 32, "diaorama9")
+
+    # Settings are as follows:
+    #            load_tiledat, load_world, tiledat_save_name, world_save_name, tileset_folder_name, tileset_prefix, tileset_depth, row, cols, rotate
+    # settings =   (True,       True,       "oceandat",         "diorama9",      "AnotherOceanMountain", "omount",     9,             32,  32,   True)
+    # settings =   (False,      True,        None,              "diaorama7",      "AnotherOceanMountain", "omount",     9,             32,  32,   True)
+    # settings =   (False,      True,        None,              "diaorama3",      "SmolMountain",         "mountain",   19,            32,  32,   True)
+    settings =   (True,       False,       "oceandat",         "diaorama10",      "AnotherOceanMountain", "omount",     9,             32,  32,   True)
+    
+    gen_size = (2,4,4)
+    chunk_size = (8,8,8)
+    tile_size = 2
+    stride = 1
+
+    load_tiledat, load_world, tiledat_save_name, world_save_name, tileset_folder_name, tileset_prefix, png_depth, png_rows, png_cols, r = settings
+
+    tileset_folder_name = "Generation_3D/images_3D/" + tileset_folder_name
+
+    presets = []
+    space_size = ((chunk_size[0] - tile_size)//stride + 1, (chunk_size[1] - tile_size)//stride + 1, (chunk_size[2] - tile_size)//stride + 1)
+
+    # Presets are specified here - 
+    presets.append(((0,0,0),0))
+
+    if load_tiledat:
+        data_tuple = gather_tilemap_data(png_depth, png_rows, png_cols, tileset_folder_name, tileset_prefix, tile_size, stride, r, None, "Saved_Data", tiledat_save_name)
+    else:
+        if tiledat_save_name is not None:
+            tiledat_save_dir = "Saved_Data"
+        else:
+            tiledat_save_dir = None
+
+        data_tuple = gather_tilemap_data(png_depth, png_rows, png_cols, tileset_folder_name, tileset_prefix, tile_size, stride, r, tiledat_save_dir, None, tiledat_save_name)
+
+    tilemap, idx_to_color, color_to_idx, tiles, weights, tile_to_dex, dex_to_tile, rev_adj, num_colors, num_states = data_tuple
+
+    if load_world:
+        space = generate_world(data_tuple, tile_size, stride, gen_size, chunk_size, presets, None, "Saved_Data", world_save_name)
+    else:
+        if world_save_name is not None:
+            world_save_dir = "Saved_Data"
+        else:
+            world_save_dir = None
+        
+        space = generate_world(data_tuple, tile_size, stride, gen_size, chunk_size, presets, world_save_dir, None, world_save_name)
+
     print("Starting Mesh!")
     create_voxel_mesh(space.tolist(), idx_to_color)
     print("Ending Mesh!")
